1d330e9648d97125f20600c1be6823bb8c5571fd
discodespot <discodespot@gmail.com>
2018-05-15 23:29:07 -0700

    home display posts
    home now displays newest posts from people you follow.

--- phplib/app/views/home.php
+++ phplib/app/views/home.php
@@ -3,6 +3,7 @@
 $user = $_SESSION['username'];
 echo "Hi $user , welcome to FuturEyes";
 $user = new User($_SESSION['usertoken']);
+echo $user->getUsername();
 
 ?>
 
@@ -28,13 +29,13 @@ if(isset($_FILES["file"]["type"]))
 			{
 				$sourcePath = $_FILES['file']['tmp_name'];// Storing source path of the file in a variable
 				$targetPath = "images/".$_FILES['file']['name']; // Target path where file is to be stored
-				if(move_uploaded_file($sourcePath,$targetPath)){  // Moving Uploaded file
-					echo "<span id='success'>Image Uploaded Successfully...!!</span><br/>";
-					echo "<br/><b>File Name:</b> " . $_FILES["file"]["name"] . "<br>";
-					echo "<b>Type:</b> " . $_FILES["file"]["type"] . "<br>";
-					echo "<b>Size:</b> " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
-					echo "<b>Temp file:</b> " . $_FILES["file"]["tmp_name"] . "<br>";
-				}
+				move_uploaded_file($sourcePath,$targetPath)) // Moving Uploaded file
+				echo "<span id='success'>Image Uploaded Successfully...!!</span><br/>";
+				echo "<br/><b>File Name:</b> " . $_FILES["file"]["name"] . "<br>";
+				echo "<b>Type:</b> " . $_FILES["file"]["type"] . "<br>";
+				echo "<b>Size:</b> " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+				echo "<b>Temp file:</b> " . $_FILES["file"]["tmp_name"] . "<br>";
+				
 			}
 		}
 	}
